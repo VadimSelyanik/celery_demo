eaa68e2246ce2a4403581cb2547a5c8672ef95f4
Vadim Selyanik <vadim.selyanik@fortegrp.com>
2021-11-19 15:57:22 +0300

    Divided tasks and celery app

--- first_step/tasks.py
+++ first_step/tasks.py
@@ -1,6 +1,13 @@
 from celery import Celery
 
 app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@localhost//')
+app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],  # Ignore other content
+    result_serializer='json',
+    timezone='Europe/Minsk',
+    enable_utc=True,
+)
 
 @app.task
 def add(x, y):
